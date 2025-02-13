ca91ee5c28d10f5de4f6dd30869d1ea3ab3bae22
jingegebuguai <jingegebuguai@gmail.com>
2025-02-13 14:00:31 +0800

    feat: yoki project config

---
+++ eslint.config.mjs
@@ -0,0 +1,117 @@
+import globals from 'globals';
+import tsParser from '@typescript-eslint/parser';
+import tsPlugin from '@typescript-eslint/eslint-plugin';
+import reactPlugin from 'eslint-plugin-react';
+import reactHooksPlugin from 'eslint-plugin-react-hooks';
+import vueParser from 'vue-eslint-parser';
+import vuePlugin from 'eslint-plugin-vue';
+import jsxA11y from 'eslint-plugin-jsx-a11y';
+
+export default [
+  // Base config for all files
+  {
+    ignores: ['dist/*', 'node_modules/*'],
+    linterOptions: {
+      reportUnusedDisableDirectives: true,
+    },
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.es2021,
+        ...globals.node,
+      },
+      parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+      },
+    },
+    rules: {
+      'no-console': 'warn',
+      'no-debugger': 'warn',
+      'no-unused-vars': 'warn',
+      'prefer-const': 'error',
+      'no-undef': 'error',
+    },
+  },
+
+  // TypeScript files
+  {
+    files: ['**/*.ts', '**/*.tsx'],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        project: './tsconfig.json',
+      },
+    },
+    plugins: {
+      '@typescript-eslint': tsPlugin,
+    },
+    rules: {
+      ...tsPlugin.configs.recommended.rules,
+      '@typescript-eslint/explicit-function-return-type': 'off',
+      '@typescript-eslint/explicit-module-boundary-types': 'off',
+      '@typescript-eslint/no-explicit-any': 'warn',
+      '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+      '@typescript-eslint/no-non-null-assertion': 'warn',
+    },
+  },
+
+  // React files
+  {
+    files: ['**/*.jsx', '**/*.tsx'],
+    plugins: {
+      react: reactPlugin,
+      'react-hooks': reactHooksPlugin,
+      'jsx-a11y': jsxA11y,
+    },
+    languageOptions: {
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+    },
+    settings: {
+      react: {
+        version: 'detect',
+      },
+    },
+    rules: {
+      ...reactPlugin.configs.recommended.rules,
+      ...reactHooksPlugin.configs.recommended.rules,
+      ...jsxA11y.configs.recommended.rules,
+      'react/prop-types': 'off',
+      'react/react-in-jsx-scope': 'off',
+      'react-hooks/rules-of-hooks': 'error',
+      'react-hooks/exhaustive-deps': 'warn',
+      'react/jsx-no-target-blank': 'warn',
+    },
+  },
+
+  // Vue files
+  {
+    files: ['**/*.vue'],
+    plugins: {
+      vue: vuePlugin,
+    },
+    languageOptions: {
+      parser: vueParser,
+      parserOptions: {
+        parser: tsParser,
+        extraFileExtensions: ['.vue'],
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+    },
+    rules: {
+      ...vuePlugin.configs.recommended.rules,
+      'vue/multi-word-component-names': 'warn',
+      'vue/no-unused-components': 'warn',
+      'vue/no-unused-vars': 'warn',
+      'vue/script-setup-uses-vars': 'error',
+      'vue/no-mutating-props': 'error',
+      'vue/component-name-in-template-casing': ['error', 'PascalCase'],
+    },
+  },
+];
---
+++ prettier.config.mjs
@@ -0,0 +1,13 @@
+export default {
+  singleQuote: true,
+  useTabs: false,
+  printWidth: 90,
+  tabWidth: 2,
+  semi: true,
+  htmlWhitespaceSensitivity: "strict",
+  arrowParens: "avoid",
+  bracketSpacing: true,
+  proseWrap: "preserve",
+  trailingComma: "all",
+  endOfLine: "lf",
+};
